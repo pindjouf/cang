d36b89e046236bc0d3e3a41e65ca2787bf9b89d7
magix <esaubukasa@protonmail.com>
2024-11-22 17:50:31 +0100

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{backend::CrosstermBackend, widgets::Paragraph, Terminal};
+use std::io;
+use std::io::stdout;
+
+fn main() -> io::Result<()> {
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.area();
+            frame.render_widget(Paragraph::new("yoooooo, press 'q' to quit."), area);
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+
+    Ok(())
 }
---
+++ src/ui/views/mod.rs
@@ -0,0 +1,4 @@
+pub mod home;
+pub mod network;
+pub mod system;
+pub mod vm;
