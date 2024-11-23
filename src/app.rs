8514baa44c49ed520327f8e27e9b53c2f393d48c
magix <esaubukasa@protonmail.com>
2024-11-23 09:36:28 +0100

    some stuff

--- src/app.rs
+++ src/app.rs
@@ -1,121 +1,140 @@
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::prelude::Constraint::{Fill, Length, Min};
-use ratatui::prelude::Stylize;
-use ratatui::DefaultTerminal;
 use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    style::Style,
-    widgets::{Block, BorderType, Borders, List, Paragraph, Tabs},
-    Frame,
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{palette::tailwind, Color, Stylize},
+    symbols,
+    text::Line,
+    widgets::{Block, BorderType, Borders, List, Padding, Paragraph, Tabs, Widget},
+    DefaultTerminal, Frame,
 };
 use std::io;
+use strum::{Display, EnumIter, FromRepr, IntoEnumIterator};
 
-struct AppState {
-    selected_tab: usize,
-    selected_item: usize,
-    is_nav_focused: bool,
+#[derive(Default)]
+pub struct App {
+    state: AppState,
+    selected_tab: SelectedTab,
 }
 
-impl AppState {
-    fn new() -> Self {
-        Self {
-            selected_tab: 1,
-            selected_item: 1,
-            is_nav_focused: true,
+#[derive(Default, Clone, Copy, PartialEq, Eq)]
+enum AppState {
+    Running,
+    Quitting,
+}
+
+#[derive(Default, Clone, Copy, Display, FromRepr, EnumIter)]
+enum SelectedTab {
+    #[default]
+    #[strum(to_string = "VMs")]
+    Tab1,
+    #[strum(to_string = "System")]
+    Tab2,
+    #[strum(to_string = "Network")]
+    Tab3,
+}
+
+impl App {
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
+        while self.state == AppState::Running {
+            terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
+            self.handle_events()?;
         }
+        Ok(())
     }
 
-    fn toggle(&mut self) {
-        if self.is_nav_focused {}
-    }
-
-    fn next(&mut self) {
-        if self.selected_tab < 2 {
-            self.selected_tab += 1;
-        } else {
-            self.selected_tab = 0;
-        };
-    }
-
-    fn previous(&mut self) {
-        if self.selected_tab > 0 {
-            self.selected_tab -= 1;
-        } else {
-            self.selected_tab = 2;
-        };
-    }
-}
-
-pub fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    let mut app_state = AppState::new();
-
-    loop {
-        terminal.draw(|frame| {
-            render(frame, &app_state);
-        })?;
-
+    fn handle_events(&mut self) -> io::Result<()> {
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Char('q') => break,
-                KeyCode::Char('l') | KeyCode::Right => app_state.next(),
-                KeyCode::Char('h') | KeyCode::Left => app_state.previous(),
+                KeyCode::Char('l') | KeyCode::Right => self.next_tab(),
+                KeyCode::Char('h') | KeyCode::Left => self.previous_tab(),
+                KeyCode::Char('q') => self.quit(),
                 _ => {}
             }
         }
+        Ok(())
     }
-    Ok(())
+
+    fn next_tab(&mut self) {
+        self.selected_tab = self.selected_tab.next();
+    }
+
+    fn previous_tab(&mut self) {
+        self.selected_tab = self.selected_tab.previous();
+    }
+
+    fn quit(&mut self) {
+        self.state = AppState::Quitting;
+    }
 }
 
-fn render(frame: &mut Frame, app_state: &AppState) {
-    let area = frame.area();
-    let titles = vec!["VMs", "System", "Network"];
-    let items = vec!["Create VM", "List VMs", "Edit VM"];
+impl SelectedTab {
+    fn previous(self) -> Self {
+        let current_index: usize = self as usize;
+        let previous_index = current_index.saturating_sub(1);
+        Self::from_repr(previous_index).unwrap_or(self)
+    }
 
-    let vertical = Layout::vertical([Length(3), Min(0), Length(1)]);
-    let [nav_area, main_area, status_area] = vertical.areas(frame.area());
-    let horizontal = Layout::horizontal([Fill(1); 2]);
-    let [left_area, right_area] = horizontal.areas(main_area);
+    fn next(self) -> Self {
+        let current_index: usize = self as usize;
+        let next_index = current_index.saturating_add(1);
+        Self::from_repr(next_index).unwrap_or(self)
+    }
+}
 
-    let block = Block::default()
-        .border_type(BorderType::Rounded)
-        .borders(Borders::ALL)
-        .title("Navigation bar");
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let area = frame.area();
+        let titles = vec!["VMs", "System", "Network"];
+        let items = vec!["Create VM", "List VMs", "Edit VM"];
 
-    let tabs = Tabs::new(titles)
-        .block(block)
-        .divider("|")
-        .select(app_state.selected_tab);
-    frame.render_widget(tabs, nav_area);
+        let vertical = Layout::vertical([Length(3), Min(0), Length(1)]);
+        let [nav_area, main_area, status_area] = vertical.areas(frame.area());
+        let horizontal = Layout::horizontal([Fill(1); 2]);
+        let [left_area, right_area] = horizontal.areas(main_area);
 
-    let content_title = match app_state.selected_tab {
-        0 => "Virtual Machines",
-        1 => "System Hardening",
-        2 => "Network Management",
-        _ => unreachable!(),
-    };
+        let block = Block::default()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::ALL)
+            .title("Navigation bar");
 
-    let preview_title = match app_state.selected_tab {
-        0 => items[0],
-        1 => items[1],
-        2 => items[2],
-        _ => unreachable!(),
-    };
+        let tabs = Tabs::new(titles)
+            .block(block)
+            .divider("|")
+            .select(app_state.selected_tab);
+        frame.render_widget(tabs, nav_area);
 
-    let list = List::new(items)
-        .block(
+        let content_title = match app_state.selected_tab {
+            Tab1 => "Virtual Machines",
+            Tab2 => "System Hardening",
+            Tab3 => "Network Management",
+            _ => unreachable!(),
+        };
+
+        let preview_title = match app_state.selected_tab {
+            Tab1 => items[0],
+            Tab2 => items[1],
+            Tab3 => items[2],
+            _ => unreachable!(),
+        };
+
+        let list = List::new(items)
+            .block(
+                Block::default()
+                    .title(content_title)
+                    .border_type(BorderType::Rounded)
+                    .borders(Borders::ALL),
+            )
+            .highlight_symbol(">>");
+        frame.render_widget(list, left_area);
+
+        let preview = Paragraph::new("yoo").block(
             Block::default()
-                .title(content_title)
+                .title(preview_title)
                 .border_type(BorderType::Rounded)
                 .borders(Borders::ALL),
-        )
-        .highlight_symbol(">>");
-    frame.render_widget(list, left_area);
-
-    let preview = Paragraph::new("yoo").block(
-        Block::default()
-            .title(preview_title)
-            .border_type(BorderType::Rounded)
-            .borders(Borders::ALL),
-    );
-    frame.render_widget(preview, right_area);
+        );
+        frame.render_widget(preview, right_area);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod app;
 
-use app::run;
+use app::App;
 use std::io;
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     terminal.clear()?;
 
-    let app_result = run(terminal);
+    let app_result = App::default().run(terminal);
     ratatui::restore();
     app_result
 }
